2d6dd9b783b13d8b72ba8bb63aa8e39b7f14b628
harshadanbhule <harshadanbhule007@gmail.com>
2024-09-17 13:29:02 +0530

    accent

---
+++ Accenture/C2W/program1.java
@@ -0,0 +1,16 @@
+//1)Count of occurrences of second largest element in array
+
+import java.util.*;
+class Demo{
+
+	public static void main(String [] args){
+		Scanner sc=new Scanner(System.in);
+		int size=sc.nextInt();
+		
+		int[] arr=new int[size];
+		for(int i=0;i<arr.length;i++){
+			arr[i]=sc.nextInt();
+		}
+
+	}
+}
