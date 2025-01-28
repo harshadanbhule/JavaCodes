66197b1eeb6663feffad0e75c677173c4642dea3
harshadanbhule <harshadanbhule007@gmail.com>
2025-01-28 13:47:27 +0530

    hola

---
+++ DSA/LinkedList/SinglyLinkedList/program1.java
@@ -0,0 +1,47 @@
+class Node{
+        int data;
+        Node next=null;
+
+        Node(int data){
+                this.data=data;
+        }
+}
+
+class LinkedList{
+        Node head=null;
+
+	void addFirst(int data){
+		Node newNode=new Node(data);
+
+		if(head==null){
+			head=newNode;
+		}else{
+			newNode.next=head;
+			head=newNode;
+		}
+	}
+
+	void printSLL(){
+		if(head==null){
+			System.out.println("Empty LinkedList");
+		}else{
+			 Node temp=head;
+
+			 while(temp!=null){
+			 	System.out.println(temp.data +" ");
+				temp =temp.next;
+			 }
+		}
+	}
+}
+
+class Client{
+
+        public static void main(String[] args){
+		LinkedList ll=new LinkedList();
+		ll.addFirst(10);
+		ll.addFirst(20);
+		ll.addFirst(30);
+		ll.printSLL();
+        }
+}
